4b33e9226446c2745ced4949568cb216186c40ec
Ngeschwi <78084247+Ngeschwi@users.noreply.github.com>
2021-12-01 16:29:39 +0100

    Add files via upload

---
+++ ex02/main.cpp
@@ -0,0 +1,17 @@
+#include <string>
+
+int	main()
+{
+	std::string		brain = "HI THIS IS BRAIN";
+	std::string*	stringPTR = &brain;
+	std::string&	stringREF = brain;
+
+	std::cout << "Address of the string : " << std::endl;
+	std::cout << &brain << std::endl;
+	std::cout << &(*stringPTR) << std::endl;
+	std::cout << &stringREF << std::endl;
+
+	std::cout << "The string :" << std::endl;
+	std::cout << *stringPTR << std::endl;
+	std::cout << stringREF << std::endl;
+}
